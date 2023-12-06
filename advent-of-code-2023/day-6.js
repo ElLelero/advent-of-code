95be9253e7ec6942744df24d10122dd3b1f55e80
ElLelero <66617539+ElLelero@users.noreply.github.com>
2023-12-06 10:31:28 +0100

    Centralized logics day 6 2023

--- advent-of-code-2023/day-6.js
+++ advent-of-code-2023/day-6.js
@@ -1,5 +1,18 @@
 const fs = require('node:fs/promises');
 
+function getWaysToBeatRecord(time, maxDistance) {
+  let delta = time * time - 4 * maxDistance;
+  if (delta >= 0) {
+    let sqrtDelta = Math.sqrt(delta);
+
+    let x1 = (time - sqrtDelta) / 2.0;
+    let x2 = (time + sqrtDelta) / 2.0;
+
+    return Math.floor(x2) - Math.floor(x1);
+  }
+  return 0;
+}
+
 function firstPart(rows) {
   let times = rows[0].substring(6).trim().split(' ').filter(n => n !== '').map(n => +n);
   let maxDistances = rows[1].substring(10).trim().split(' ').filter(n => n !== '').map(n => +n);
@@ -7,13 +20,7 @@ function firstPart(rows) {
   let waysToBeatTheRecord = null;
 
   for (let i = 0; i < times.length; i++) {
-    let time = times[i];
-
-    let sqrtDelta = Math.sqrt(time * time - 4 * maxDistances[i]);
-    let x1 = (-time + sqrtDelta) / -2.0;
-    let x2 = (-time - sqrtDelta) / -2.0;
-
-    waysToBeatTheRecord = (waysToBeatTheRecord ?? 1) * (Math.floor(x2) - Math.floor(x1));
+    waysToBeatTheRecord = (waysToBeatTheRecord ?? 1) * getWaysToBeatRecord(times[i], maxDistances[i]);
   }
   return waysToBeatTheRecord ?? 0;
 }
@@ -22,12 +29,7 @@ function secondPart(rows) {
   let time = +(rows[0].substring(6).trim().split(' ').join(''));
   let maxDistance = +(rows[1].substring(10).trim().split(' ').join(''));
 
-  let sqrtDelta = Math.sqrt(time * time - 4 * maxDistance);
-
-  let x1 = (-time + sqrtDelta) / -2.0;
-  let x2 = (-time - sqrtDelta) / -2.0;
-
-  return Math.floor(x2) - Math.floor(x1);
+  return getWaysToBeatRecord(time, maxDistance);
 }
 
 fs.readFile('./advent-of-code-2023/inputs/day-6.txt', { encoding: 'utf8' }).then((data) => {
