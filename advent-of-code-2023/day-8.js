11dd6affecb1a5886c5cab62311931b4903cab43
ElLelero <66617539+ElLelero@users.noreply.github.com>
2023-12-08 13:04:36 +0100

    Refactored day 8 2023

--- advent-of-code-2023/day-8.js
+++ advent-of-code-2023/day-8.js
@@ -28,36 +28,37 @@ function firstPart(rows) {
 function secondPart(rows) {
   let commands = rows[0];
 
+  let currentNodes = [], nodesSteps = [];
   let nodes = {};
   for (let i = 2; i < rows.length; i++) {
     let [node, destinationNodes] = rows[i].split(' = ');
     let [leftNode, rightNode] = destinationNodes.substring(1, destinationNodes.length - 1).split(', ');
 
     nodes[node] = [leftNode, rightNode];
+
+    if (node.endsWith('A')) {
+      currentNodes.push(node);
+      nodesSteps.push(0);
+    }
   }
 
-  let currentNodes = [...new Set(Object.keys(nodes))].filter(node => node.endsWith('A'));
-  let endingSteps = new Array(currentNodes.length).fill(0);
-  let steps = 0;
-  while (true) {
-    for (let i = 0; i < commands.length; i++) {
-      for (let j = 0; j < currentNodes.length; j++) {
-        let index = commands[i] === 'L' ? 0 : 1;
-        currentNodes[j] = nodes[currentNodes[j]][index];
-        if (endingSteps[j] === 0 && currentNodes[j].endsWith('Z')) {
-          endingSteps[j] = steps + 1;
-        }
-      }
-      steps++;
-      if (!endingSteps.some(s => s === 0)) {
-        let lcmEndingSteps = undefined;
-        for (let j = 0; j < endingSteps.length - 1; j++) {
-          lcmEndingSteps = lcm(lcmEndingSteps ?? endingSteps[j], endingSteps[j + 1]);
-        }
-        return lcmEndingSteps;
+  for (let i = 0; i < currentNodes.length; i++) {
+    let steps = 0;
+    let found = false;
+    while (!found) {
+      for (let j = 0; j < commands.length && !found; j++) {
+        let index = commands[j] === 'L' ? 0 : 1;
+        currentNodes[i] = nodes[currentNodes[i]][index];
+        nodesSteps[i] = ++steps;
+        found = currentNodes[i].endsWith('Z');
       }
     }
   }
+  let concurrentSteps = nodesSteps[0];
+  for (let j = 1; j < nodesSteps.length; j++) {
+    concurrentSteps = lcm(concurrentSteps, nodesSteps[j]);
+  }
+  return concurrentSteps;
 }
 
 function gcd(a, b) {
